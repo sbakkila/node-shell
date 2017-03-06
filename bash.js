0a2fd0abd82db7fa868751c0ce4e72f2bea89e87
Sam Bakkila <sbakkila@gmail.com>
2017-03-06 12:32:58 -0500

    added cat, head, and tail

--- bash.js
+++ bash.js
@@ -6,14 +6,26 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
   if(cmd === 'pwd'){
-    command.pwd();
+    command.pwd(cmd);
 
   }
   if(cmd === "date"){
-    command.date();
+    command.date(cmd);
   }
   if (cmd === "ls") {
-    command.ls();
+    command.ls(cmd);
+  }
+  if (cmd.slice(0, 4) === "echo"){
+    command.echo(cmd.slice(4));
+  }
+  if(cmd.slice(0, 3) === 'cat'){
+    command.cat(cmd.slice(4));
+  }
+  if(cmd.slice(0, 4) === 'head'){
+    command.head(cmd.slice(5));
+  }
+  if(cmd.slice(0, 4) === 'tail'){
+    command.tail(cmd.slice(5));
   }
   process.stdout.write('\nprompt > ');
 
--- command.js
+++ command.js
@@ -1,20 +1,50 @@
 var fs = require('fs');
 
 module.exports = {
-  pwd: function(){
+  pwd: function(cmd){
     process.stdout.write(process.env.PWD);
   },
-  date: function(){
+  date: function(cmd){
     var date = new Date(Date.now()).toString();
     process.stdout.write(date);
   },
-  ls: function(){
+  ls: function(cmd){
     fs.readdir('.', function(err, files) {
       if (err) throw err;
       files.forEach(function(file) {
         process.stdout.write(file.toString() + "\n");
       })
     process.stdout.write("prompt > ");
+    });
+  },
+  echo: function(str){
+    process.stdout.write(str);
+  },
+  cat: function(filename){
+    fs.readFile('./' + filename, 'utf8', (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  },
+  head: function(filename){
+    fs.readFile('./' + filename, 'utf8', (err, data) => {
+      var strData = data.toString();
+
+      if (err) throw err;
+      var firstFive = strData.split("\n").slice(0,5).join("\n");
+
+      console.log(firstFive);
+    });
+
+  },
+  tail: function(filename){
+    fs.readFile('./' + filename, 'utf8', (err, data) => {
+      var strData = data.toString();
+
+      if (err) throw err;
+      var firstFive = strData.split("\n").slice(-5).join("\n");
+
+      console.log(firstFive);
     });
   }
 
